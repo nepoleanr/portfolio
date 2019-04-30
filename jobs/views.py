e6f604aa00d1b86d8e282204cbc61dcd85639922
Nepolean Ramesvaran <nepoleanr.apple@gmail.com>
2019-04-30 21:20:39 +0530

    Added bootsrap. Made changes for the job app for its visibility online

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
+def home(request):
+    jobs = Job.objects  # this fetches all the Job objects from the DB and converts them into Python objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
