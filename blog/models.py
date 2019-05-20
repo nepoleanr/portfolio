3f8ebc0ea8f9c180f40c73f2a5b8537d0f7da204
Nepolean Ramesvaran <nepoleanr.apple@gmail.com>
2019-05-20 20:46:16 +0530

    Added Blog functionality and final touches

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     published_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        stringified_body = str(self.body)
+        return stringified_body[:100] + "..."
+
+
+    def pub_date_pretty(self):
+        return self.published_date.strftime('%b %e %Y')
+    
+    def __str__(self):
+        return self.title
